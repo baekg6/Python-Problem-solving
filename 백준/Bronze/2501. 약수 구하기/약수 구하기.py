ae7711558f17c5bbc039636293139c11fe4bbfc5
Gyeonghyeon Baek <96777064+baekg6@users.noreply.github.com>
2022-09-01 23:22:48 +0900

    [Bronze III] Title: 약수 구하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+divisors = []
+for i in range(1, n+1):
+    if n%i==0:
+        divisors.append(i)
+
+
+if k <= len(divisors):
+    print(divisors[k-1])
+else:
+    print(0)    
